ed82cb4cb1fbe40572275ce1cf879f1e32600c85
SophyManwa <sophykwamboka24@gmail.com>
2023-05-24 18:02:50 +0000

    Update on the codes

--- executecmd.c
+++ executecmd.c
@@ -1,33 +1,39 @@
 #include "simple_shell.h"
 
-/* Function to execute the user input command*/
+/**
+ * execute_command - Function to execute the user input command
+ * @cmd_path: Command path
+ * @args: Command arguments
+ *
+ * Return: On success, return NULL
+ */
 
-void execute_command(char *cmd_path, char **args) 
+
+void execute_command(char *cmd_path, char **args)
 {
 	pid_t child_process;
 	int status;
 
 /*Forking the child process*/
-	child_process = fork();
-
-    if (child_process == -1)
-    {
-        perror("Failed to fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (child_process == 0)
-    {
-/* Execute command using exceve*/
-	    if (execve(cmd_path, args, NULL) == -1)
+	child_process = fork()
+	if (child_process == -1)
 	{
-            perror("Failed to execute");
-            exit(EXIT_FAILURE);
-        }
-       /* exit(EXIT_SUCCESS);*/
-    }
-    else
-    {
+		perror("Failed to fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_process == 0)
+	{
+/* Execute command using exceve*/
+		if (execve(cmd_path, args, NULL) == -1)
+		{
+			perror("Failed to execute");
+			exit(EXIT_FAILURE);
+		}
+/* exit(EXIT_SUCCESS);*/
+	}
+	else
+	{
 /*Parent process*/
-	    waitpid(child_process, &status, 0);
-    }
+		waitpid(child_process, &status, 0);
+	}
 }
