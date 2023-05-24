7a490735c82925bac024bbb01fdff0e0b8db9e2b
SophyManwa <sophykwamboka24@gmail.com>
2023-05-24 19:04:45 +0000

    update

--- simple_shell.h
+++ simple_shell.h
@@ -17,6 +17,7 @@ char *strcpy(char *dest, const char *src);
 char *strcat(char *dest, const char *src);
 size_t string_length(const char *str);
 int strcmp(const char *str1, const char *str2);
+int strncmp(const char *s1, const char *s2, size_t c);
 
 /*Function to get the environment*/
 char *getenv(const char *name);
