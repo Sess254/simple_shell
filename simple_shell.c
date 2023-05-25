74e80bf70d50a93acced0b9a4825608b47db9806
Sess254 <wamaritemoses@gmail.com>
2023-05-25 12:18:43 +0300

    update

--- simple_shell.c
+++ simple_shell.c
@@ -1,23 +1,23 @@
 #include "simple_shell.h"
 /**
  * main - Main entry point
+ * @argv: argument vector
+ * @argc: argument counter
  * path_env: environment variable for the PATH
  * strtok: function to tokenize the PATH Variable
  * cmd_path: hold the path of the command
  * strcmp: function to compare the user input with exit command
  * Return: On sucess, 0.
  */
-int main(void)
+int main(int argc, char **argv)
 {
 	char *user_input, **args, *path, *cmd_path, *path_env, **paths;
+	char *error_message = "No such file or directory\n";
 	int count = 0;/*count of path*/
-	/* Getting the PATH environment variable*/
+	(void)argc;
 	path_env = getenv("PATH");
-	/*Memory Allocation to paths*/
 	paths = malloc(BUFFER_SIZE * sizeof(char *));
-	/*Tokenizining the PATH variable*/
 	path = strtok(path_env, ":");
-	/*while*/
 	while (path != NULL)
 	{
 		paths[count] = path;
@@ -38,7 +38,9 @@ int main(void)
 		cmd_path = find_command(args[0], paths);
 		if (cmd_path == NULL)
 		{
-			write(STDOUT_FILENO, "No such file or directory\n", 26);
+			write(STDOUT_FILENO, argv[0], strlen(argv[0]));
+			write(STDOUT_FILENO, ": ", 2);
+			write(STDOUT_FILENO, error_message, strlen(error_message));
 		}
 		else
 		{
